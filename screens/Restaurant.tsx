8c07614e3e600f8698d51216449f8b4e026ce04c
NatsuyaKume <65702108+natsuya-kume@users.noreply.github.com>
2021-08-11 15:23:31 +0900

    [add] #1 setup screens

---
+++ screens/Restaurant.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {View, Text} from 'react-native';
+
+const Restaulant = () => {
+  return (
+    <View>
+      <Text>Restaulant</Text>
+    </View>
+  );
+};
+
+export default Restaulant;
